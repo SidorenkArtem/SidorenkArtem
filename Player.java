ecdc598a300a529f93de5178cce28e65c7e10e46
SidorenkArtem <54068383+SidorenkArtem@users.noreply.github.com>
2019-08-22 18:49:44 +0300

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,60 @@
+class Player {
+
+    class Human extends Player{
+
+         private char element;
+
+
+         public Human(){}
+
+         public void setElement(char element) {
+             this.element = element;
+         }
+
+         public char getElement() {
+             return element;
+         }
+
+         public String toString() {
+             return "I am a human!";
+         }
+
+    }
+
+     class Bot extends Player{
+
+         private char element;
+         private int max_random = 9;
+
+         public Bot(){}
+
+         public void setElement(char element) {
+             this.element = element;
+         }
+
+         public char getElement() {
+             return element;
+         }
+
+
+         int botPosition(char[] array_of_positions) {
+             int value;
+             while (true) {
+                 value = (int) (Math.random() * max_random );
+                 if (freePosition(array_of_positions, value))
+                     return value;
+             }
+
+         }
+
+        boolean freePosition(char[] array_of_positions, int index){
+            return array_of_positions[index] == ' ' ? true : false;
+        }
+
+         public String toString(){
+             return "I am a bot!";
+         }
+
+
+    }
+}
